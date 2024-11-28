506aede85c862beec71e049228ac5da307b51a39
محمد السيد رضوان عبد العال حسين <es.Mohamed.radwan2023@alexu.edu.eg>
2024-11-28 16:30:00 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//TODO: printing the main menu
+void print_menu() {
+    
+}
+//TODO: sign up
+
+//TODO: log in
+
+//TODO: printing doctors data
+
+
+
+int main() {
+    //print main menu
+
+    //take operation
+
+    
+}
